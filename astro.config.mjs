74a8e829e35ef66462a064607b28483eb71a8015
joeally06 <62224411+joeally06@users.noreply.github.com>
2025-05-08 19:29:17 -0500

    Update Astro dependencies and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
+import sharp from '@astrojs/sharp';
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
+    sharp()
   ]
 });
